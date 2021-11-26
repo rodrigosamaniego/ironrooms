ad2f8abda59706d6ed17ae94c9b518c3730a5d90
rodrigosamaniego <rodrigosamaniegopiza@gmail.com>
2021-11-26 10:21:14 -0600

    segundo commit

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const connectDB = async () => {
+    await mongoose.connect(process.env.MONGODB_URI,{
+        useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+
+	console.log("Base de datos conectada")
+
+}
+
+module.exports = connectDB
---
+++ index.js
@@ -0,0 +1,25 @@
+// 1. IMPORTACIONES
+const express 	= require("express")
+const { connect } = require("mongoose")
+const app		= express()
+const hbs = require("hbs")
+
+const connectDB = require("./config/db")
+
+require("dotenv").config()
+
+// 2. MIDDLEWARES
+app.use(express.static("public"))
+app.set("views", __dirname + "/views")
+
+app.set("view engine", "hbs")
+
+connectDB()
+
+// 3. RUTAS
+app.use("/", require(("./routes/index")))
+
+// 4. SERVIDOR
+app.listen(process.env.PORT, () => {
+	console.log(`Servidor conectado en el puerto ${process.env.PORT}`)
+})
